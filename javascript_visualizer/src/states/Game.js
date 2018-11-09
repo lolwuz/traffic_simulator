47f50090518bafd57a52589fe1e832b655ac744d
lolwuz <martenhoekstra2@gmail.com>
2018-11-09 13:39:48 +0100

    Fixed merger

--- javascript_visualizer/src/states/Game.js
+++ javascript_visualizer/src/states/Game.js
@@ -6,7 +6,6 @@ import {QuadTree} from 'phaser-ce'
 import trajectory from '../trajectory'
 
 export default class Game extends Phaser.State {
-<<<<<<< HEAD
   /** Phaser State functions */
   preload () {
     this.isDown = false
@@ -16,25 +15,19 @@ export default class Game extends Phaser.State {
 
     this.cursor = this.game.input.keyboard.createCursorKeys()
     this.camera = this.game.camera
+    this.game.input.mouse.capture = true
+    this.isDown = false
+
+    this.points = []
+    this.lines = []
+
+    // this.debugPoints(console.log())
   }
 
   create () {
     this.roadMap = this.game.add.sprite(0, 0, 'road_map')
     this.game.world.setBounds(0, 0, this.roadMap.width, this.roadMap.height)
-=======
-  preload () {
-    this.game.load.image('road_map', './assets/images/road_map_small.png')
-    this.game.load.image('top_car', './assets/images/top_car.png')
-    this.game.load.image('centroid', '/assets/images/centroid.png')
-    this.isDown = false
-    this.points = []
-    this.over = false
-    this.currentPoint
-  }
-
-  create () {
-    let roadMap = this.game.add.sprite(0, 0, 'road_map')
->>>>>>> 74baf472f3e44d24f256b01c9fc2e9a7f2ca703a
+    this.game.camera.focusOnXY(this.roadMap.width / 2, this.roadMap.height / 2)
 
     this.testCar = new Trafic({
       game: this.game,
@@ -51,38 +44,53 @@ export default class Game extends Phaser.State {
     this.currentPoint = this.game.add.image(0, 0, 'centroid')
     this.currentPoint.anchor.set(0.5)
     this.currentPoint.alpha = 0.5
-
-    this.game.input.onTap.add(this.onTapHandler, this)
   }
 
-  onTapHandler () {
-    console.log(this.over)
-    if (!this.over) {
-      let img = this.game.add.sprite(this.game.input.activePointer.position.x, this.game.input.activePointer.position.y, 'centroid', 0)
-      this.points.push(img.position)
+  render () {
+    for (let i = 0; i < this.lines.length; i++) {
+      this.game.debug.geom(this.lines[i])
+    }
+  }
 
-      img.anchor.set(0.5)
-      img.alpha = 0.25
-      img.inputEnabled = true
-      img.input.enableDrag(true)
-      img.defaultCursor = 'move'
+  debugPoints () {
+    this.game.input.onDown.add(this.mousePressed, this)
+    let spacebar = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR)
+    spacebar.onDown.add(this.spacePressed, this)
+  }
 
-      img.events.onInputOver.add(function () {
-        this.alpha = 1
-        this.scale.setTo(1.2, 1.2)
-        this.over = true
-      }, img)
+  mousePressed (point) {
+    let position = new Phaser.Point(point.worldX, point.worldY)
+    this.points.push(position)
 
-<<<<<<< HEAD
+    this.lines = []
+
+    let lastPoint = new Phaser.Point(0, 0)
+    for (let i = 0; i < this.points.length; i++) {
+      let currentPoint = this.points[i]
+      if (lastPoint.x !== 0) {
+        let line = new Phaser.Line(lastPoint.x, lastPoint.y, currentPoint.x, currentPoint.y)
+        this.lines.push(line)
+      }
+      lastPoint = currentPoint
+    }
+  }
+
+  spacePressed () {
+    console.log(this.points)
+
+    this.lines = []
+    this.points = []
+  }
+
+  update () {
+    let pointer = this.game.input.activePointer
+
+    if (pointer.isDown && !this.isDown) {
+      this.isDown = true
+    }
+
     if (pointer.isUp && this.isDown) {
       this.isDown = false
-=======
-      img.events.onInputOut.add(function () {
-        this.alpha = 0.25
-        this.scale.setTo(1, 1)
-        this.over = false
-      }, img)
->>>>>>> 74baf472f3e44d24f256b01c9fc2e9a7f2ca703a
     }
 
     this.updateScale(pointer)
@@ -126,26 +134,5 @@ export default class Game extends Phaser.State {
 
     this.lastX = pointer.x
     this.lastY = pointer.y
-  }
-
-  update () {
-    this.currentPoint.position.copyFrom(this.game.input.activePointer.position)
-    // let pointer = this.game.input.activePointer
-    //
-    // if (pointer.isDown && !this.isDown) {
-    //   this.isDown = true
-    // }
-    //
-    // if (pointer.isUp && this.isDown) {
-    //   console.log('click')
-    //   this.isDown = false
-    // }
-  }
-
-  render () {
-    this.game.world.forEachAlive(function (child) {
-      this.game.debug.text(Phaser.Math.roundTo(child.x, 0) + ',' + Phaser.Math.roundTo(child.y, 0),
-        child.x - 10, child.y + 25, '#ff1e00', '12px Courier')
-    }.bind(this))
   }
 }
